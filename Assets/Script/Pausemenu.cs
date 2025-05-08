20f706e8fd89c20b29bb2ee5374ec97b7b355cbe
ajdinkulo <ajdin@kulo.me>
2025-05-08 19:57:04 +0200

    Made a pausemenu

---
+++ Assets/Script/Pausemenu.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Pausemenu : MonoBehaviour
+{
+    public GameObject PauseMenu;
+    public bool isPaused;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+    public void PauseGame()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+}
